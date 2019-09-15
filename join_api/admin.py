2ecf67afd5d237183f51bc7e1c03cff7518d445f
dayeong <sdy2856@gmail.com>
2019-09-15 19:19:45 +0900

    made join_tattist table

--- join_api/admin.py
+++ join_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from join_api.models import join_tattist
 
 # Register your models here.
+admin.site.register(join_tattist)
--- join_api/models.py
+++ join_api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class join_tattist(models.Model):
+    tatt_work = models.CharField(max_length=300)    # 시술사진
+    tatt_addr = models.CharField(max_length=200)    #타티스트 주소
+    tatt_intro = models.CharField(max_length=200)   #타티스트 간단 소개
+    tatt_time = models.CharField(max_length=200)    #타티스트 작업 가능 시간
+    tatt_date = models.CharField(max_length=200)    #타티스트 작업 가능 요일
+    tatt_profile = models.CharField(max_length=300) #타티스트 프로필 사진
+
+    
