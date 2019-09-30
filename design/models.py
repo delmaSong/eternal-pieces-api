11551cc9b3329e009d1c4ba210f98fe80e098753
dayeong <sdy2856@gmail.com>
2019-10-01 01:13:10 +0900

    Modified tiny changes with fk relations at design/models.py

--- design/models.py
+++ design/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-from join_api.models import Join_tattist
+from login_api.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
 class Design(models.Model):
     design_name = models.CharField(max_length=500) #도안 이름
-    design_photho = models.ImageField(upload_to="designs")
-    design_price = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(5)])
-    design_size = models.IntegerField(default=0)
-    design_spent_time = models.IntegerField(default=0)
+    design_price = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    design_size = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    design_spent_time = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
     design_style = models.CharField(max_length=200)
     design_desc = models.CharField(max_length=1000)
-    tatt_id = models.ForeignKey(Join_tattist, on_delete=models.CASCADE, db_column = 'tatt_id')
+    tatt_id = models.ForeignKey(User, on_delete=models.CASCADE, db_column = 'tatt_id')
+    design_photo = models.ImageField(upload_to="designs")
